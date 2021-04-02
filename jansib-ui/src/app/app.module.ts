581263a05a7e62686ebd0e8ad9fbc487221b93df
Javi Casanova <nhitbh@gmail.com>
2021-04-02 21:49:25 +0200

    remove unused component

--- jansib-ui/src/app/app.module.ts
+++ jansib-ui/src/app/app.module.ts
@@ -5,12 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectModule } from './project/project.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
