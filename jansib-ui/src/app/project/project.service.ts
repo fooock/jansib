c9fa1cfe65b3fc452bf412c39fd9d51220ac0403
Javi Casanova <nhitbh@gmail.com>
2021-04-02 16:00:28 +0200

    add methods to add and retrieve projects

--- jansib-ui/src/app/project/project.service.ts
+++ jansib-ui/src/app/project/project.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Project } from './project';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
+  private projects: Project[] = [];
+
   constructor(private client: HttpClient) { }
+
+  getProjects(): Observable<Project[]> {
+    return of(this.projects);
+  }
+
+  addProject(project: Project): void {
+    this.projects.push(project);
+  }
 }
